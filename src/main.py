cf1b08cb0811135a20788f1571ec3b11e5deca1f
Shin Kranel <shinkranel@gmail.com>
2024-05-27 07:33:49 +0100

    Little changes

--- src/main.py
+++ src/main.py
@@ -1,22 +1,31 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from src.operations.router import router as operations_router
+from src.budget.router import router as budget_router
+from src.user.router import router as user_router
 
 app = FastAPI()
 
 
-@app.get("/unprotected-route")
+@app.get("/")
 def protected_route():
     return f"Hello, anonym!"
 
 
 app.include_router(operations_router, prefix="/operations", tags=["operations"])
+app.include_router(budget_router, prefix="/budget", tags=["budget"])
+app.include_router(user_router, prefix="/user", tags=["user"])
+
+
+@app.get("/app")
+def read_main(request: Request):
+    return {"message": "Hello World", "root_path": request.scope.get("root_path")}
 
 
 if __name__ == "__main__":
     uvicorn.run(
-        "src.main:app",
+        "main:app",
         host='localhost',
         port=8000,
         reload=True
