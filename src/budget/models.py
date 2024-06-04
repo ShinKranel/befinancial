b0c0dfa0eaae90a9820d3eb11a31e7c52bd3e740
Shin Kranel <shinkranel@gmail.com>
2024-06-04 19:49:51 +0100

    Update budget models

--- src/budget/models.py
+++ src/budget/models.py
@@ -11,8 +11,12 @@ class Budget(Base):
     __tablename__ = "budget"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+
     value: Mapped[float]
+
+    # foreign keys
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"))
 
-    user: Mapped["User"] = relationship(back_populates="budgets")
-    operations: Mapped[list["Operation"]] = relationship(back_populates="budget")
+    # relationships
+    user = relationship("User", back_populates="budgets")
+    operations = relationship("Operation", back_populates="budget")
